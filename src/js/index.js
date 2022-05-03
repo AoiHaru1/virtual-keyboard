40e8d21c3a75a12be984d2418a5611bd995a5f3e
Aoi Haru <snakeinwater1@gmail.com>
2022-05-03 18:24:25 +0300

    feat: add json data that contains info about the keys

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 import '../assets/css/style.css';
 import ContainerTemplateCreator from './modules/containersTemplate';
 
+const data = require('./data/keysData.json');
+
 ContainerTemplateCreator();
